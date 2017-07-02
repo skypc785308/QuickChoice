081ba8756e9a4bb952e79fafa316ed3964160caf
ChiaMing Chang <skypc785308@gmail.com>
2017-07-02 23:24:01 +0800

    改寫登入介面

--- app/src/main/java/com/mis/ncyu/quickchoice/MainActivity.java
+++ app/src/main/java/com/mis/ncyu/quickchoice/MainActivity.java
@@ -2,6 +2,9 @@ package com.mis.ncyu.quickchoice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       Button btn = (Button) findViewById(R.id.button3);
+        btn.setOnClickListener(btnlisten);
     }
+
+    View.OnClickListener btnlisten = new View.OnClickListener() {
+        public void onClick(View v){
+            Toast.makeText(MainActivity.this,"大家好",Toast.LENGTH_SHORT).show();
+        }
+    };
+
 }
